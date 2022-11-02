84cdff64fdfcbded0c8693b6293fab8236fc2c67
jiwonkim97 <jiwon.kim@int2.us>
2022-11-02 17:11:55 +0900

    update: navigation, recoil

---
+++ mudkip/src/navigator/DrawerAppNavigator.js
@@ -0,0 +1,13 @@
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import Main from '../Screens/Main';
+
+const Drawer = createDrawerNavigator();
+
+function DrawerAppNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Main" component={Main} />
+    </Drawer.Navigator>
+  );
+}
+export default DrawerAppNavigator;
